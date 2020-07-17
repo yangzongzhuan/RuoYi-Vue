7af68609e6ec7d121e3f378db2522ad50da59c76
GoRoad <ghy12345@outlook.com>
2020-07-17 15:39:39 +0800

    :bug: 修改注释

--- ruoyi/src/main/java/com/ruoyi/project/tool/gen/util/GenUtils.java
+++ ruoyi/src/main/java/com/ruoyi/project/tool/gen/util/GenUtils.java
@@ -198,7 +198,7 @@ public class GenUtils
     /**
      * 关键字替换
      * 
-     * @param name 需要被替换的名字
+     * @param text 需要被替换的关键字
      * @return 替换后的名字
      */
     public static String replaceText(String text)
