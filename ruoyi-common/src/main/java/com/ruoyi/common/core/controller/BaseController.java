01b545e4abd20a92cb239b9d43f22881696a4b2a
RuoYi <yzz_ivy@163.com>
2021-03-16 14:43:55 +0800

    通用Controller添加响应返回消息

--- ruoyi-common/src/main/java/com/ruoyi/common/core/controller/BaseController.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/controller/BaseController.java
@@ -85,6 +85,49 @@ public class BaseController
     }
 
     /**
+     * 响应返回结果
+     * 
+     * @param result 结果
+     * @return 操作结果
+     */
+    protected AjaxResult toAjax(boolean result)
+    {
+        return result ? success() : error();
+    }
+
+    /**
+     * 返回成功
+     */
+    public AjaxResult success()
+    {
+        return AjaxResult.success();
+    }
+
+    /**
+     * 返回失败消息
+     */
+    public AjaxResult error()
+    {
+        return AjaxResult.error();
+    }
+
+    /**
+     * 返回成功消息
+     */
+    public AjaxResult success(String message)
+    {
+        return AjaxResult.success(message);
+    }
+
+    /**
+     * 返回失败消息
+     */
+    public AjaxResult error(String message)
+    {
+        return AjaxResult.error(message);
+    }
+
+    /**
      * 页面跳转
      */
     public String redirect(String url)
