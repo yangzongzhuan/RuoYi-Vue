32f333a00e3222b75be61ecb6e9de3900cfa96c4
RuoYi <yzz_ivy@163.com>
2021-05-11 14:25:39 +0800

    删除操作日志记录日志

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/SysOperlogController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/SysOperlogController.java
@@ -48,6 +48,7 @@ public class SysOperlogController extends BaseController
         return util.exportExcel(list, "操作日志");
     }
 
+    @Log(title = "操作日志", businessType = BusinessType.DELETE)
     @PreAuthorize("@ss.hasPermi('monitor:operlog:remove')")
     @DeleteMapping("/{operIds}")
     public AjaxResult remove(@PathVariable Long[] operIds)
