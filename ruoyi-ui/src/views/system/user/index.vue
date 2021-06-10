ea66e20282e5dc62a477e52d3edc90d24367c472
RuoYi <yzz_ivy@163.com>
2021-06-10 22:36:22 +0800

    修复用户搜索分页变量错误

--- ruoyi-ui/src/views/system/user/index.vue
+++ ruoyi-ui/src/views/system/user/index.vue
@@ -545,7 +545,7 @@ export default {
     },
     /** 搜索按钮操作 */
     handleQuery() {
-      this.queryParams.page = 1;
+      this.queryParams.pageNum = 1;
       this.getList();
     },
     /** 重置按钮操作 */
