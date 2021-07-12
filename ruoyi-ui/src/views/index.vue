170cfb356c93c32fa804799c4bc9d02b83b21dae
RuoYi <yzz_ivy@163.com>
2021-07-12 08:42:11 +0800

    若依 3.6.0

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -147,6 +147,45 @@
             <span>更新日志</span>
           </div>
           <el-collapse accordion>
+            <el-collapse-item title="v3.6.0 - 2021-07-12">
+              <ol>
+                <li>角色管理新增分配用户功能</li>
+                <li>用户管理新增分配角色功能</li>
+                <li>日志列表支持排序操作</li>
+                <li>优化参数&字典缓存操作</li>
+                <li>系统布局配置支持动态标题开关</li>
+                <li>菜单路由配置支持内链访问</li>
+                <li>默认访问后端首页新增提示语</li>
+                <li>富文本默认上传返回url类型</li>
+                <li>新增自定义弹窗拖拽指令</li>
+                <li>全局注册常用通用组件</li>
+                <li>全局挂载字典标签组件</li>
+                <li>ImageUpload组件支持多图片上传</li>
+                <li>FileUpload组件支持多文件上传</li>
+                <li>文件上传组件添加数量限制属性</li>
+                <li>富文本编辑组件添加类型属性</li>
+                <li>富文本组件工具栏配置视频</li>
+                <li>封装通用iframe组件</li>
+                <li>限制超级管理员不允许操作</li>
+                <li>用户信息长度校验限制</li>
+                <li>分页组件新增pagerCount属性</li>
+                <li>添加bat脚本执行应用</li>
+                <li>升级oshi到最新版本v5.7.4</li>
+                <li>升级element-ui到最新版本2.15.2</li>
+                <li>升级pagehelper到最新版1.3.1</li>
+                <li>升级commons.io到最新版本v2.10.0</li>
+                <li>升级commons.fileupload到最新版本v1.4</li>
+                <li>升级swagger到最新版本v3.0.0</li>
+                <li>修复关闭confirm提示框控制台报错问题</li>
+                <li>修复存在的SQL注入漏洞问题</li>
+                <li>定时任务屏蔽rmi远程调用</li>
+                <li>修复用户搜索分页变量错误</li>
+                <li>修复导出角色数据范围翻译缺少仅本人</li>
+                <li>修复表单构建选择下拉选择控制台报错问题</li>
+                <li>优化图片工具类读取文件</li>
+                <li>其他细节优化</li>
+              </ol>
+            </el-collapse-item>
             <el-collapse-item title="v3.5.0 - 2021-05-25">
               <ol>
                 <li>新增菜单导航显示风格TopNav（false为左侧导航菜单，true为顶部导航菜单）</li>
@@ -566,7 +605,7 @@ export default {
   data() {
     return {
       // 版本号
-      version: "3.5.0",
+      version: "3.6.0",
     };
   },
   methods: {
