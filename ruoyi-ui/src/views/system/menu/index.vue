fc60c003c39139c450c1445b146a13ee4ee3f30b
RuoYi <yzz_ivy@163.com>
2021-08-06 14:31:39 +0800

    升级element-ui到最新版本2.15.5

--- ruoyi-ui/src/views/system/menu/index.vue
+++ ruoyi-ui/src/views/system/menu/index.vue
@@ -88,7 +88,7 @@
     </el-table>
 
     <!-- 添加或修改菜单对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="600px" append-to-body>
+    <el-dialog :title="title" :visible.sync="open" width="680px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="100px">
         <el-row>
           <el-col :span="24">
