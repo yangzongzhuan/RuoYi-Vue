519ea854d57f83feb6d6e992ce85eaad0a9acaaa
RuoYi <yzz_ivy@163.com>
2021-10-15 17:57:46 +0800

    生产环境使用路由懒加载提升页面响应速度

--- ruoyi-ui/src/store/modules/permission.js
+++ ruoyi-ui/src/store/modules/permission.js
@@ -106,8 +106,13 @@ function filterChildren(childrenMap, lastRouter = false) {
   return children
 }
 
-export const loadView = (view) => { // 路由懒加载
-  return (resolve) => require([`@/views/${view}`], resolve)
+export const loadView = (view) => {
+  if (process.env.NODE_ENV === 'development') {
+    return (resolve) => require([`@/views/${view}`], resolve)
+  } else {
+    // 使用 import 实现生产环境的路由懒加载
+    return () => import(`@/views/${view}`)
+  }
 }
 
 export default permission
