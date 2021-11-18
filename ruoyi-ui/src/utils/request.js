fcf606acde83e67d050bc0758cce5960892f3904
RuoYi <yzz_ivy@163.com>
2021-11-18 17:50:49 +0800

    升级jsencrypt到最新版本3.2.1

--- ruoyi-ui/src/utils/request.js
+++ ruoyi-ui/src/utils/request.js
@@ -48,19 +48,16 @@ service.interceptors.response.use(res => {
       return res.data
     }
     if (code === 401) {
-      let doms = document.getElementsByClassName('el-message-box')[0]
-      if(doms === undefined){
-        MessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', {
+      MessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', {
           confirmButtonText: '重新登录',
           cancelButtonText: '取消',
           type: 'warning'
         }
-        ).then(() => {
-          store.dispatch('LogOut').then(() => {
-            location.href = '/index';
-          })
-        }).catch(() => {});
-      }
+      ).then(() => {
+        store.dispatch('LogOut').then(() => {
+          location.href = '/index';
+        })
+      }).catch(() => {});
       return Promise.reject('无效的会话，或者会话已过期，请重新登录。')
     } else if (code === 500) {
       Message({
