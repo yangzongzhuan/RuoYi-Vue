a966b95a5b26c3201b498aa2889f2d3c2f52532b
SG <462682667@qq.com>
2022-01-14 09:24:15 +0000

    update ruoyi-quartz/src/main/java/com/ruoyi/quartz/controller/SysJobController.java. 修正文字

--- ruoyi-quartz/src/main/java/com/ruoyi/quartz/controller/SysJobController.java
+++ ruoyi-quartz/src/main/java/com/ruoyi/quartz/controller/SysJobController.java
@@ -128,7 +128,7 @@ public class SysJobController extends BaseController
         }
         else if (StringUtils.containsAnyIgnoreCase(job.getInvokeTarget(), new String[] { Constants.LOOKUP_LDAP, Constants.LOOKUP_LDAPS }))
         {
-            return error("修改任务'" + job.getJobName() + "'失败，目标字符串不允许'ldap'调用");
+            return error("修改任务'" + job.getJobName() + "'失败，目标字符串不允许'ldap(s)'调用");
         }
         else if (StringUtils.containsAnyIgnoreCase(job.getInvokeTarget(), new String[] { Constants.HTTP, Constants.HTTPS }))
         {
