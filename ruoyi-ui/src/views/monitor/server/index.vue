54c6c4e547252cb14c3b0fe1a08758e42fd97691
RuoYi <yzz_ivy@163.com>
2022-02-20 16:06:33 +0800

    服务监控新增运行参数信息显示

--- ruoyi-ui/src/views/monitor/server/index.vue
+++ ruoyi-ui/src/views/monitor/server/index.vue
@@ -128,6 +128,10 @@
                   <td colspan="1" class="el-table__cell is-leaf"><div class="cell">项目路径</div></td>
                   <td colspan="3" class="el-table__cell is-leaf"><div class="cell" v-if="server.sys">{{ server.sys.userDir }}</div></td>
                 </tr>
+                <tr>
+                  <td colspan="1" class="el-table__cell is-leaf"><div class="cell">运行参数</div></td>
+                  <td colspan="3" class="el-table__cell is-leaf"><div class="cell" v-if="server.sys">{{ server.jvm.inputArgs }}</div></td>
+                </tr>
               </tbody>
             </table>
           </div>
