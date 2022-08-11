890ad682d7cd179fd5ce6b52515146ee9329ea7b
RuoYi <yzz_ivy@163.com>
2022-08-11 13:22:55 +0800

    优化修改资料头像被覆盖的问题(I5LK04)

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysProfileController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysProfileController.java
@@ -75,6 +75,7 @@ public class SysProfileController extends BaseController
         }
         user.setUserId(sysUser.getUserId());
         user.setPassword(null);
+        user.setAvatar(null);
         if (userService.updateUserProfile(user) > 0)
         {
             // 更新缓存用户信息
