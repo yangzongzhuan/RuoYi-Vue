b970185536bc58742fc29946b50c487a2de52bac
kknd97 <liujingwei@ln.chinamobile.com>
2022-10-20 06:27:02 +0000

    update ruoyi-ui/src/views/system/user/index.vue. handleUpdate(row)方法中：this.form = response.data;语句 会导致【this.form.postIds = response.postIds;】和【this.form.roleIds = response.roleIds;】失效。 导致用户编辑对话框中，角色和部门多选框无法正常修改。 建议使用以下语句修改： this.$set(this.form, "postIds", response.postIds); this.$set(this.form, "roleIds", response.roleIds);
    Signed-off-by: kknd97 <liujingwei@ln.chinamobile.com>

--- ruoyi-ui/src/views/system/user/index.vue
+++ ruoyi-ui/src/views/system/user/index.vue
@@ -576,8 +576,8 @@ export default {
         this.form = response.data;
         this.postOptions = response.posts;
         this.roleOptions = response.roles;
-        this.form.postIds = response.postIds;
-        this.form.roleIds = response.roleIds;
+        this.$set(this.form, "postIds", response.postIds);
+        this.$set(this.form, "roleIds", response.roleIds);
         this.open = true;
         this.title = "修改用户";
         this.form.password = "";
