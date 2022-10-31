101e15d83f4b91ef338d2cbc3be43fe1176cc779
也曾为你像超人 <1553592282@qq.com>
2022-10-31 05:45:21 +0000

    修复调度日志点击多次数据不变化的问题
    Signed-off-by: 也曾为你像超人 <1553592282@qq.com>

--- ruoyi-ui/src/views/monitor/job/index.vue
+++ ruoyi-ui/src/views/monitor/job/index.vue
@@ -454,7 +454,7 @@ export default {
     /** 任务日志列表查询 */
     handleJobLog(row) {
       const jobId = row.jobId || 0;
-      this.$router.push({ path: '/monitor/job-log/index', query: { jobId: jobId } })
+      this.$router.push('/monitor/job-log/index/' + jobId)
     },
     /** 新增按钮操作 */
     handleAdd() {
