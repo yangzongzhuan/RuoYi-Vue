27acbe5b73794e7d703c6a3c7fc34b961c26da4a
RuoYi <yzz_ivy@163.com>
2022-11-22 09:23:17 +0800

    修复某些特性的环境生成代码变乱码TXT文件问题

--- ruoyi-ui/src/views/tool/gen/index.vue
+++ ruoyi-ui/src/views/tool/gen/index.vue
@@ -267,7 +267,7 @@ export default {
           this.$modal.msgSuccess("成功生成到自定义路径：" + row.genPath);
         });
       } else {
-        this.$download.zip("/tool/gen/batchGenCode?tables=" + tableNames, "ruoyi");
+        this.$download.zip("/tool/gen/batchGenCode?tables=" + tableNames, "ruoyi.zip");
       }
     },
     /** 同步数据库操作 */
