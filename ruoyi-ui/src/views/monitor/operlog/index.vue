6523fe59a299b2f159b0793deab73271fd65f463
RuoYi <yzz_ivy@163.com>
2023-02-23 10:01:16 +0800

    日志管理使用索引提升查询性能

--- ruoyi-ui/src/views/monitor/operlog/index.vue
+++ ruoyi-ui/src/views/monitor/operlog/index.vue
@@ -53,11 +53,12 @@
         <el-date-picker
           v-model="dateRange"
           style="width: 240px"
-          value-format="yyyy-MM-dd"
+          value-format="yyyy-MM-dd HH:mm:ss"
           type="daterange"
           range-separator="-"
           start-placeholder="开始日期"
           end-placeholder="结束日期"
+          :default-time="['00:00:00', '23:59:59']"
         ></el-date-picker>
       </el-form-item>
       <el-form-item>
