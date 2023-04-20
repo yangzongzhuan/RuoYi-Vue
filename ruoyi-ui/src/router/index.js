eff06c110f49db6c9632b53deb5ea4b6a926d4b2
loren-li <loren-li@outlook.com>
2023-04-20 15:02:38 +0800

    修复路由跳转被阻止时vue-router内部产生报错信息问题

--- ruoyi-ui/src/router/index.js
+++ ruoyi-ui/src/router/index.js
@@ -166,8 +166,14 @@ export const dynamicRoutes = [
 
 // 防止连续点击多次路由报错
 let routerPush = Router.prototype.push;
+let routerReplace = Router.prototype.replace;
+// push
 Router.prototype.push = function push(location) {
   return routerPush.call(this, location).catch(err => err)
+}
+// replace
+Router.prototype.replace = function push(location) {
+  return routerReplace.call(this, location).catch(err => err)
 }
 
 export default new Router({
