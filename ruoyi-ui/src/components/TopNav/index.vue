4a78fe116d21e6ccfd94c82a838a6b111f07dc96
RuoYi <yzz_ivy@163.com>
2023-08-31 10:18:25 +0800

    优化TopNav菜单没有图标svg不显示

--- ruoyi-ui/src/components/TopNav/index.vue
+++ ruoyi-ui/src/components/TopNav/index.vue
@@ -6,7 +6,10 @@
   >
     <template v-for="(item, index) in topMenus">
       <el-menu-item :style="{'--theme': theme}" :index="item.path" :key="index" v-if="index < visibleNumber"
-        ><svg-icon :icon-class="item.meta.icon" />
+        ><svg-icon
+          v-if="item.meta && item.meta.icon && item.meta.icon !== '#'"
+          :icon-class="item.meta.icon"
+        />
         {{ item.meta.title }}</el-menu-item
       >
     </template>
