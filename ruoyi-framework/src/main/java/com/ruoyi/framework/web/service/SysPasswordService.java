3f4ac65a31b8d24f05a349b799759cada0c9eb2a
RuoYi <yzz_ivy@163.com>
2023-12-13 11:51:17 +0800

    remove packages

--- ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/SysPasswordService.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/SysPasswordService.java
@@ -6,15 +6,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import com.ruoyi.common.constant.CacheConstants;
-import com.ruoyi.common.constant.Constants;
 import com.ruoyi.common.core.domain.entity.SysUser;
 import com.ruoyi.common.core.redis.RedisCache;
 import com.ruoyi.common.exception.user.UserPasswordNotMatchException;
 import com.ruoyi.common.exception.user.UserPasswordRetryLimitExceedException;
-import com.ruoyi.common.utils.MessageUtils;
 import com.ruoyi.common.utils.SecurityUtils;
-import com.ruoyi.framework.manager.AsyncManager;
-import com.ruoyi.framework.manager.factory.AsyncFactory;
 import com.ruoyi.framework.security.context.AuthenticationContextHolder;
 
 /**
