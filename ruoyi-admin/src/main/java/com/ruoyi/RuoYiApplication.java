de503d52fe8b8f6d6b5e776472ac4a1530d36f7c
zhouym <zym16621@163.com>
2024-07-08 14:25:28 +0800

    1.修改启动后的显示信息：增加启动的地址和端口号以及swagger的地址 2.修改数据库连接地址及用户名密码

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -1,8 +1,15 @@
 package com.ruoyi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * 启动程序
@@ -12,19 +19,37 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class RuoYiApplication
 {
-    public static void main(String[] args)
+    private static Logger logger = LoggerFactory.getLogger(RuoYiApplication.class);
+
+    public static void main(String[] args) throws UnknownHostException
     {
-        // System.setProperty("spring.devtools.restart.enabled", "false");
-        SpringApplication.run(RuoYiApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+        ConfigurableApplicationContext application = SpringApplication.run(RuoYiApplication.class, args);
+
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String active = env.getProperty("spring.profiles.active");
+        String port = env.getProperty("server.port");
+        port = port == null ? "8080" : port;
+        String path = env.getProperty("server.servlet.context-path");
+        path = path == null ? "" : path;
+        String xxlJobAddresses = env.getProperty("xxl.job.admin.addresses");
+        xxlJobAddresses = xxlJobAddresses == null ? "" : xxlJobAddresses;
+        logger.info("\n----------------------------------------------------------\n\t" +
+                "系统运行环境 : \t" +active + "\n\t" +
+                "本地访问地址 : \thttp://localhost:" + port + path + "\n\t" +
+                "外部访问地址 : \thttp://" + ip + ":" + port + path + "\n\t" +
+                "在线文档地址 : \thttp://" + ip + ":" + port + path + "swagger-ui/index.html\n\t" +
+                "----------------------------------------------------------");
+
+//        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+//                " .-------.       ____     __        \n" +
+//                " |  _ _   \\      \\   \\   /  /    \n" +
+//                " | ( ' )  |       \\  _. /  '       \n" +
+//                " |(_ o _) /        _( )_ .'         \n" +
+//                " | (_,_).' __  ___(_ o _)'          \n" +
+//                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+//                " |  | \\ `'   /|   `-'  /           \n" +
+//                " |  |  \\    /  \\      /           \n" +
+//                " ''-'   `'-'    `-..-'              ");
     }
 }
