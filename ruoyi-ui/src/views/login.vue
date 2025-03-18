5a03a754e8ddef3c66402795f7d81789574d3719
Chingfeng Li <qingfeng.li001@pantum.local>
2025-03-18 14:53:46 +0800

    登录页肯注册页表头使用VUE_APP_TITLE配置值

--- ruoyi-ui/src/views/login.vue
+++ ruoyi-ui/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">若依后台管理系统</h3>
+      <h3 class="title">{{title}}</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
@@ -70,6 +70,7 @@ export default {
   name: "Login",
   data() {
     return {
+      title: process.env.VUE_APP_TITLE,
       codeUrl: "",
       loginForm: {
         username: "admin",
