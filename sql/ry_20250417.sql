84fef1f675f6f05427a2b3f1874c627718de3087
RuoYi <yzz_ivy@163.com>
2025-04-17 15:07:38 +0800

    update status name

--- sql/ry_20250417.sql
+++ sql/ry_20250417.sql
@@ -50,7 +50,7 @@ create table sys_user (
   sex               char(1)         default '0'                comment '用户性别（0男 1女 2未知）',
   avatar            varchar(100)    default ''                 comment '头像地址',
   password          varchar(100)    default ''                 comment '密码',
-  status            char(1)         default '0'                comment '帐号状态（0正常 1停用）',
+  status            char(1)         default '0'                comment '账号状态（0正常 1停用）',
   del_flag          char(1)         default '0'                comment '删除标志（0代表存在 2代表删除）',
   login_ip          varchar(128)    default ''                 comment '最后登录IP',
   login_date        datetime                                   comment '最后登录时间',
