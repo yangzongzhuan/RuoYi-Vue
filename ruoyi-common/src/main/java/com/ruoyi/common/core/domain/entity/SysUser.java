3d09ea31fce41ef96b3ace3864ad238196d79b0c
RuoYi <yzz_ivy@163.com>
2025-04-17 15:24:08 +0800

    update status name

--- ruoyi-common/src/main/java/com/ruoyi/common/core/domain/entity/SysUser.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/domain/entity/SysUser.java
@@ -55,8 +55,8 @@ public class SysUser extends BaseEntity
     /** 密码 */
     private String password;
 
-    /** 帐号状态（0正常 1停用） */
-    @Excel(name = "帐号状态", readConverterExp = "0=正常,1=停用")
+    /** 账号状态（0正常 1停用） */
+    @Excel(name = "账号状态", readConverterExp = "0=正常,1=停用")
     private String status;
 
     /** 删除标志（0代表存在 2代表删除） */
