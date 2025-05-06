6cafa3373e6140feaaf9f1dceb8037ee747c717e
稚屿 <1491182878@qq.com>
2025-05-06 04:53:32 +0000

    修复图片上传组件在同一页面中被多次引用时，仅有第一个组件拖拽功能生效的问题
    Signed-off-by: 稚屿 <1491182878@qq.com>

--- ruoyi-ui/src/components/ImageUpload/index.vue
+++ ruoyi-ui/src/components/ImageUpload/index.vue
@@ -104,7 +104,7 @@ export default {
   mounted() {
     if (this.drag) {
       this.$nextTick(() => {
-        const element = document.querySelector('.el-upload-list')
+        const element = this.$refs.imageUpload?.$el?.querySelector('.el-upload-list')
         Sortable.create(element, {
           onEnd: (evt) => {
             const movedItem = this.fileList.splice(evt.oldIndex, 1)[0]
