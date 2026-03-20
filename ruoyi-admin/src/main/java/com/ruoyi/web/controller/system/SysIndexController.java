cd0ee0a5cdea0b2238a677ba2347ca200f429251
RuoYi <yzz_ivy@163.com>
2026-03-20 20:36:44 +0800

    新增锁定屏幕功能

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysIndexController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysIndexController.java
@@ -1,10 +1,17 @@
 package com.ruoyi.web.controller.system;
 
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ruoyi.common.config.RuoYiConfig;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.domain.entity.SysUser;
+import com.ruoyi.common.utils.SecurityUtils;
 import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.system.service.ISysUserService;
 
 /**
  * 首页
@@ -18,6 +25,9 @@ public class SysIndexController
     @Autowired
     private RuoYiConfig ruoyiConfig;
 
+    @Autowired
+    private ISysUserService userService;
+
     /**
      * 访问首页，提示语
      */
@@ -25,5 +35,30 @@ public class SysIndexController
     public String index()
     {
         return StringUtils.format("欢迎使用{}后台管理框架，当前版本：v{}，请通过前端地址访问。", ruoyiConfig.getName(), ruoyiConfig.getVersion());
+    }
+
+    /**
+     * 解锁屏幕
+     */
+    @PostMapping("/unlockscreen")
+    public AjaxResult unlockScreen(@RequestBody Map<String, String> body)
+    {
+        String password = body.get("password");
+        if (StringUtils.isEmpty(password))
+        {
+            return AjaxResult.error("密码不能为空");
+        }
+        String username = SecurityUtils.getUsername();
+        SysUser user = userService.selectUserByUserName(username);
+        if (user == null)
+        {
+            return AjaxResult.error("服务器超时，请重新登录");
+        }
+        if (!SecurityUtils.matchesPassword(password, user.getPassword()))
+        {
+            return AjaxResult.error("密码错误，请重新输入");
+        }
+
+        return AjaxResult.success("解锁成功");
     }
 }
--- ruoyi-ui/src/layout/components/Navbar.vue
+++ ruoyi-ui/src/layout/components/Navbar.vue
@@ -45,6 +45,9 @@
           <el-dropdown-item @click.native="setLayout" v-if="setting">
             <span>布局设置</span>
           </el-dropdown-item>
+          <el-dropdown-item @click.native="lockScreen">
+            <span>锁定屏幕</span>
+          </el-dropdown-item>
           <el-dropdown-item divided @click.native="logout">
             <span>退出登录</span>
           </el-dropdown-item>
@@ -111,6 +114,12 @@ export default {
     },
     setLayout(event) {
       this.$emit('setLayout')
+    },
+    lockScreen() {
+      const currentPath = this.$route.fullPath
+      this.$store.dispatch('lock/lockScreen', currentPath).then(() => {
+        this.$router.push('/lock')
+      })
     },
     logout() {
       this.$confirm('确定注销并退出系统吗？', '提示', {
